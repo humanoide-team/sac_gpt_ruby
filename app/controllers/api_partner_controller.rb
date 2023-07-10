e12af83be69f46b54488fc42bbf02459b418f9a9
Dayana <dayanadiasgarcia@gmail.com>
2023-07-10 19:34:33 -0300

    authentication from partner

--- app/controllers/api_partner_controller.rb
+++ app/controllers/api_partner_controller.rb
@@ -1,4 +1,6 @@
 class ApiPartnerController < ActionController::Base
+  include HTTParty
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :null_session
