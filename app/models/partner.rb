84df108fb8aadcaa3aad7d99f9cbc2a69a71baeb
Esdras Oliveira <esdrasjesus5@gmail.com>
2023-07-14 16:58:28 -0300

    fix: instance_key generation

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -16,9 +16,26 @@ class Partner < ApplicationRecord
 
   accepts_nested_attributes_for :partner_detail, reject_if: :all_blank
 
+  after_save :generate_instance_key, unless: :instance_key?
+
   def name_slug
     return unless name.present?
 
     ["#{name.parameterize}"]
   end
+
+  def generate_key
+    SecureRandom.random_bytes(32)
+  end
+
+  def encrypted_data(data)
+    crypt = ActiveSupport::MessageEncryptor.new(generate_key)
+    crypt.encrypt_and_sign(data)
+  end
+
+  def generate_instance_key
+    token = encrypted_data(self.id.to_s)
+    self.update_attribute(:instance_key, token)
+  end
+
 end
