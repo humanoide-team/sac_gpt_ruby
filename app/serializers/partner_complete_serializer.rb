6ee5a42bab486938efac092aad73e1f07bd4399b
Dayana <dayanadiasgarcia@gmail.com>
2023-08-02 16:00:02 -0300

    update serializer and webhook controller

--- app/serializers/partner_complete_serializer.rb
+++ app/serializers/partner_complete_serializer.rb
@@ -2,6 +2,6 @@ class PartnerCompleteSerializer
   include FastJsonapi::ObjectSerializer
   set_key_transform :camel_lower
 
-  attributes :name, :email, :service_number, :document, :partner_detail, :created_at, :updated_at
+  attributes :name, :email, :service_number, :contact_number, :document, :partner_detail, :created_at, :updated_at
 
 end
--- app/serializers/partner_serializer.rb
+++ app/serializers/partner_serializer.rb
@@ -2,7 +2,7 @@ class PartnerSerializer
   include FastJsonapi::ObjectSerializer
   set_key_transform :camel_lower
 
-  attributes :name, :email, :service_number, :document, :partner_detail_id, :created_at, :updated_at
+  attributes :name, :email, :service_number, :contact_number, :document, :partner_detail_id, :created_at, :updated_at
   attributes :auth_token, :expires_at, if: Proc.new { |record| record.auth_token.present? }
 
 end
