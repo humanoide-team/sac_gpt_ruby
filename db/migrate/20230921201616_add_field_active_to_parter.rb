646811dba120e2f052f86300507497c8e56e26d5
Esdras Oliveira <esdrasjesus5@gmail.com>
2023-09-21 17:23:19 -0300

    fix: fixed migration active

--- db/migrate/20230921201616_add_field_active_to_parter.rb
+++ db/migrate/20230921201616_add_field_active_to_parter.rb
@@ -1,5 +1,5 @@
 class AddFieldActiveToParter < ActiveRecord::Migration[6.1]
   def change
-    add_column :parters, :active, :boolean, default: false
+    add_column :partners, :active, :boolean, default: false
   end
 end
