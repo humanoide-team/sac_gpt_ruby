f4673a4c984172f903e34d5c32ff9ae1fecf0512
Esdras Oliveira <esdrasjesus5@gmail.com>
2023-10-10 16:35:37 -0300

    fix: fix prompt

--- app/models/partner_detail.rb
+++ app/models/partner_detail.rb
@@ -7,7 +7,7 @@ class PartnerDetail < ApplicationRecord
     Nossos principais objetivos incluem #{main_goals}, com metas específicas em #{business_goals}. Oferecemos uma gama de serviços, como #{company_services}, 
     e produtos, incluindo #{company_products}. 
     Utilize nossos canais de marketing, como #{marketing_channels}, e visite nosso site em #{company_contact}. 
-    Nosso grande diferencial é #{keydifferentials}, o que nos diferencia na concorrência e nos permite entregar resultados notáveis. 
+    Nosso grande diferencial é #{key_differentials}, o que nos diferencia na concorrência e nos permite entregar resultados notáveis. 
     Sua comunicação deve estar alinhada com o público-alvo da empresa, 
     utilizando um tom de voz #{tone_voice}. Concentre-se em identificar as necessidades específicas e os desafios do cliente, fazendo no máximo uma pergunta por mensagem e mantendo as respostas curtas, não ultrapassando 50 palavras. 
     Após entender claramente as necessidades do cliente, ou caso o cliente solicite, proponha o #{company_objective}. Responda apenas na linguagem #{preferential_language}"
