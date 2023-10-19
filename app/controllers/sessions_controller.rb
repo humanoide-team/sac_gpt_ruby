5d21da080174afabb46bfaced934af3bd0d6a8fe
Esdras Oliveira <esdrasjesus5@gmail.com>
2023-10-19 18:00:03 -0300

    fix: errors create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,22 @@
 class SessionsController < ApplicationController
   def create
     user_data = request.env['omniauth.auth']
-    session[:token] = user_data['credentials']['token']
 
-    @partner = Partner.find(params['partnerId'])
+    unless user_data.nil?
+      session[:token] = user_data['credentials']['token']
 
-    unless @partner.nil?
-      @partner.access_token = user_data.credentials.token
-      @partner.expires_at = user_data.credentials.expires_at
-      @partner.refresh_token = user_data.credentials.refresh_token
-      @partner.save!
+      @partner = Partner.find(params['partnerId'])
 
-      return render json: { message: 'Conta conectada' }, status: 200
+      unless @partner.nil?
+        @partner.access_token = user_data.credentials.token
+        @partner.expires_at = user_data.credentials.expires_at
+        @partner.refresh_token = user_data.credentials.refresh_token
+        @partner.save!
+
+        return render json: { message: 'Conta conectada' }, status: 200
+      end
+      render json: { error: 'Usuário não existe' }, status: 401
     end
-    render json: { error: 'Usuário não existe' }, status: 401
+    render json: { error: 'Token expirado' }, status: 401
   end
 end
