98fb8916c1c5f0b1f8649536d744ea983dc39f96
Esdras Oliveira <esdrasjesus5@gmail.com>
2023-10-23 13:38:24 -0300

    fix: partner notifications

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -19,6 +19,7 @@ class Partner < ApplicationRecord
   has_many :payment_subscriptions, dependent: :destroy
   has_many :partner_client_leads, dependent: :destroy
   has_many :partner_client_conversation_infos, dependent: :destroy
+  has_many :notifications, dependent: :destroy
 
   validates :name, :service_number, :document, :contact_number, presence: true
   validates :password_confirmation, presence: true, on: :create
