cc84324c8d99c96fb0e18e9ff1e2f1f03d774e4a
Esdras Oliveira <esdrasjesus5@gmail.com>
2023-10-31 16:20:59 -0300

    fix: company_objectives

--- app/serializers/partner_detail_serializer.rb
+++ app/serializers/partner_detail_serializer.rb
@@ -2,5 +2,5 @@ class PartnerDetailSerializer
   include FastJsonapi::ObjectSerializer
   set_key_transform :camel_lower
 
-  attributes :name_attendant, :company_name, :company_niche, :served_region, :company_products, :company_services, :company_contact, :company_objective, :main_goals, :business_goals, :marketing_channels, :key_differentials, :target_audience, :tone_voice, :preferential_language, :created_at, :updated_at
+  attributes :name_attendant, :company_name, :company_niche, :served_region, :company_products, :company_services, :company_contact, :company_objectives, :main_goals, :business_goals, :marketing_channels, :key_differentials, :target_audience, :tone_voice, :preferential_language, :created_at, :updated_at
 end
