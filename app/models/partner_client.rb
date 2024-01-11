4f1122f6ddc76b803f1a4ad11adb66c4629ec735
Esdras Oliveira <esdrasjesus5@gmail.com>
2024-01-11 17:17:58 -0300

    fix: partner_client destroy

--- app/models/partner_client.rb
+++ app/models/partner_client.rb
@@ -3,5 +3,5 @@ class PartnerClient < ApplicationRecord
   has_many :partner_client_messages, dependent: :destroy
   has_many :partner_client_leads, dependent: :destroy
   has_many :partner_client_conversation_infos, dependent: :destroy
-
+  has_many :schedules, dependent: :destroy
 end
