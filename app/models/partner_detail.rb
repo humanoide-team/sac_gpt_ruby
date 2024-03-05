4c924abecef7f68a4dba438acab241ab297c9252
Esdras Oliveira <esdrasjesus5@gmail.com>
2024-03-05 18:22:23 -0300

    feat: change prompt

--- app/models/partner_detail.rb
+++ app/models/partner_detail.rb
@@ -13,7 +13,7 @@ class PartnerDetail < ApplicationRecord
       "Os serviços oferecidos são #{company_services}, e os produtos são #{company_products}." +
       "Estes são nossos canais de marketing, como #{marketing_channels}, e nosso contato #{company_contact}. Além disso, oferecemos mais informações em nosso [site do negócio]." +
       "Nosso grande diferencial é #{key_differentials}. E, a menos que instruído de outra forma, você responderá na língua #{preferential_language}." +
-      'Identifique as necessidades específicas e os desafios do cliente e faça no máximo uma pergunta por mensagem e mantendo as respostas curtas, não ultrapassando 50 palavras e responda com a formatação apropriada para o WhatsApp.' +
+      'Identifique as necessidades específicas e os desafios do cliente e faça no máximo uma pergunta por mensagem e mantendo as respostas curtas, não ultrapassando 50 palavras e responda com a formatação apropriada para o WhatsApp, divida a resposta em blocos de texto para melhorar a leiturabilidade das mensagens.' +
       "Após entender claramente as necessidades do cliente, proponha o #{company_objectives.join(', ')}. #{catalog_link.nil? ? '' : "Quando alguem solicitar o catálogo envie o link #{catalog_link}."} Responda 'Peço desculpas, mas não posso fornecer essa informação' quando não souber responder a informação exata. Responda no formato de mensagem de texto para whatsapp."
   end
 
