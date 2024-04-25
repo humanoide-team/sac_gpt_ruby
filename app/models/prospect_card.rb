702cb34598fcd579d44b42c1cd7b1cda258eeff3
Esdras Oliveira <esdrasjesus5@gmail.com>
2024-04-25 19:02:00 -0300

    fix: fixed prospectCard

--- app/models/prospect_card.rb
+++ app/models/prospect_card.rb
@@ -1,5 +1,5 @@
 class ProspectCard < ApplicationRecord
-  has_one :prospect_details, dependent: :destroy
+  has_one :prospect_detail, dependent: :destroy
   belongs_to :prospect_card
 
   accepts_nested_attributes_for :prospect_detail, reject_if: :all_blank
