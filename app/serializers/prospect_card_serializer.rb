c0cb14801e061240daa62782ec3281e05bb6c41c
Esdras Oliveira <esdrasjesus5@gmail.com>
2024-05-20 12:43:29 -0300

    fix: fixed propect card serializer

--- app/serializers/prospect_card_serializer.rb
+++ app/serializers/prospect_card_serializer.rb
@@ -2,7 +2,7 @@ class ProspectCardSerializer
   include FastJsonapi::ObjectSerializer
   set_key_transform :camel_lower
 
-  attributes :name,:company_name, :phone, :email, :observations, :status, :prospect_details
+  attributes :name,:company_name, :phone, :email, :observations, :status, :prospect_detail
 
   attribute :partner_linked do |o|
     o.partner_linked
