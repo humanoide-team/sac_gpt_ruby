f2f7a7ce6ef27dc5f046959897cac6e77fff2eb0
Esdras Oliveira <esdrasjesus5@gmail.com>
2024-06-03 18:39:10 -0300

    refact: revenue controller index

--- app/controllers/api/v1/affiliates/revenues_controller.rb
+++ app/controllers/api/v1/affiliates/revenues_controller.rb
@@ -8,7 +8,22 @@ class Api::V1::Affiliates::RevenuesController < ApiAffiliateController
     financial_start = financial_date.beginning_of_month
     financial_end = financial_date.end_of_month
 
-    revenues = @current_affiliate.revenues
+    revenues = @current_affiliate.revenues.map do |r|
+      {
+        partner: {
+          id: r.partner.id,
+          name: r.partner.name
+        },
+        value: r.value,
+        createdAt: r.created_at,
+        updatedAt: r.updated_at,
+        partner_transaction: {
+          id: r.partner_transaction_id,
+          type: r.partner_transaction_type
+        }
+      }
+    end
+
     closing = @current_affiliate.revenues.where(created_at: closing_start..closing_end).sum(:value)
     financial = @current_affiliate.revenues.where(created_at: financial_start..financial_end).sum(:value)
 
